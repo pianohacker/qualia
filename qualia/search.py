a48633504c84d9e9fe751512a623186ae8d481b4
Jesse Weaver <pianohacker@gmail.com>
2014-12-24 12:28:30 -0700

    Start of support for field aliases

--- qualia/search.py
+++ qualia/search.py
@@ -1,5 +1,6 @@
 from . import config, common
 
+import copy
 from whoosh import analysis, fields, index, qparser, query
 
 def _create_field_type(field_config):
@@ -11,6 +12,57 @@ def _create_field_type(field_config):
 		number = fields.NUMERIC(stored = True),
 		text = fields.TEXT(analyzer = analysis.StemmingAnalyzer(), stored = True),
 	)[field_config['type'].replace('-', '_')]
+
+class _FieldAliasPlugin(qparser.Plugin):
+	# Based on whoosh.qparser.CopyFieldPlugin, which has the following license:
+	#
+	#> Copyright 2011 Matt Chaput. All rights reserved.
+	#>
+	#> Redistribution and use in source and binary forms, with or without
+	#> modification, are permitted provided that the following conditions are met:
+	#>
+	#>    1. Redistributions of source code must retain the above copyright notice,
+	#>       this list of conditions and the following disclaimer.
+	#>
+	#>    2. Redistributions in binary form must reproduce the above copyright
+	#>       notice, this list of conditions and the following disclaimer in the
+	#>       documentation and/or other materials provided with the distribution.
+	#>
+	#> THIS SOFTWARE IS PROVIDED BY MATT CHAPUT ``AS IS'' AND ANY EXPRESS OR
+	#> IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+	#> MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+	#> EVENT SHALL MATT CHAPUT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+	#> INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	#> LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+	#> OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	#> LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	#> NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+	#> EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	#>
+	#> The views and conclusions contained in the software and documentation are
+	#> those of the authors and should not be interpreted as representing official
+	#> policies, either expressed or implied, of Matt Chaput.
+    def __init__(self, map):
+        self.map = map
+
+    def filters(self, parser):
+        # Run after the fieldname filter (100) but before multifield (110)
+        return [(self.do_fieldalias, 109)]
+
+    def do_fieldalias(self, parser, group):
+        map = self.map
+        newgroup = group.empty_copy()
+        for node in group:
+            if isinstance(node, qparser.syntax.GroupNode):
+                # Recurse into groups
+                node = self.do_fieldalias(parser, node)
+            elif node.has_fieldname:
+                fname = node.fieldname or parser.fieldname
+                if fname in map:
+                    node = copy.copy(node)
+                    node.set_fieldname(map[fname], override=True)
+            newgroup.append(node)
+        return newgroup
 
 class SearchDatabase:
 	def __init__(self, db, base_path):
@@ -30,6 +82,8 @@ class SearchDatabase:
 
 			self.index = index.create_in(base_path, schema)
 
+		self.field_alias_map = {}
+
 	def add(self, hash):
 		writer = self.index.writer()
 		# We use update_document, rather than add_document, so this function can be mostly
@@ -48,8 +102,11 @@ class SearchDatabase:
 
 	def parse_query(self, query):
 		parser = qparser.QueryParser('comments', self.index.schema)
+		parser.add_plugin(qparser.GtLtPlugin())
 		parser.add_plugin(qparser.WildcardPlugin())
-		parser.replace_plugin(qparser.FieldsPlugin(expr=r"(?P<text>[\w-]+|[*]):"))
+		parser.replace_plugin(qparser.FieldsPlugin(expr=r"(?P<text>[\w-]+|[*]):", remove_unknown = False))
+		parser.add_plugin(_FieldAliasPlugin(self.field_alias_map))
+		print(parser.process(query))
 
 		return parser.parse(query)
 
