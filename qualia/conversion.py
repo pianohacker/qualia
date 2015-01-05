b13e57a8d2d8bc1ad36211bfec247a5559716eee
Jesse Weaver <pianohacker@gmail.com>
2015-01-04 20:15:58 -0700

    Add GPLv3 info

--- qualia/conversion.py
+++ qualia/conversion.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2015 Jesse Weaver.
+#
+# This file is part of Qualia.
+# 
+# Qualia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Qualia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Qualia. If not, see <http://www.gnu.org/licenses/>.
+
 # This file contains a number of utility functions for converting metadata to/from qualia's internal
 # formats, text entered by the user or filesystem/embedded metadata.
 #
