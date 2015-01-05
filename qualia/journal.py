807c0eec3c7351fed2c54aa1d212ed994d73f96b
Jesse Weaver <pianohacker@gmail.com>
2015-01-04 18:51:08 -0700

    Slight cleanup and documentation of journal

--- qualia/journal.py
+++ qualia/journal.py
@@ -1,27 +1,40 @@
+# This is the implementation of the journal, which allows for recovery and limited undo.
+#
+## Imports
 import base64
 import datetime
 import pickle
 import sqlite3
 
+## Journal
+# The journal is implemented on top of a simple SQLite database, which gives us a convenient
+# datastore and good disaster resilience.
 class Journal:
 	def __init__(self, filename):
 		self.db = sqlite3.connect(
 			filename,
+			# This will use the column types (not much more than a hint to SQLite) to do conversion
+			# to and from Python types.
 			detect_types = sqlite3.PARSE_DECLTYPES
 		)
+		# Make SQLite use a write-ahead instead of a delete-based journal; see
+		# [the SQLite documentation](https://www.sqlite.org/wal.html) for more info.
+		self.db.execute('PRAGMA journal_mode=WAL');
+
+		# This replaces the usual tuple format for rows with a convenient dict-like object.
 		self.db.row_factory = sqlite3.Row
 		self.upgrade_if_needed()
-		self.f = open(filename, 'ab')
+
+		# This indicates whether there have been changes since the last checkpoint.
 		self.has_changes = False
 
 	def upgrade_if_needed(self):
-		# Make SQLite use a write-ahead instead of a delete-based journal; see
-		# https://www.sqlite.org/wal.html for more info.
-		self.db.execute('PRAGMA journal_mode=WAL;');
+		# We check the version of the database and upgrade it if necessary.
+		# Conveniently, this starts at 0 in an empty database.
 		version = self.db.execute('PRAGMA user_version').fetchone()[0]
 
-		if version < 1:
-			self.db.executescript("""
+		updates = [
+			"""
 				CREATE TABLE journal (
 					serial INTEGER PRIMARY KEY,
 					timestamp TIMESTAMP,
@@ -30,19 +43,24 @@ class Journal:
 					op TEXT,
 					extra BLOB
 				);
-			""")
-
-		if version < 2:
-			self.db.executescript("""
+			""",
+			"""
 				CREATE TABLE checkpoints (
 					checkpoint_id INTEGER PRIMARY KEY,
 					timestamp TIMESTAMP,
 					serial INTEGER
 				);
-			""")
+			"""
+		]
 
-		self.db.execute("PRAGMA user_version = 2")
+		# We set the `user_version` after each update to ensure updates are not applied twice if one
+		# in a sequence of updates fails.
+		for version, update in enumerate(updates[version:], version + 1):
+			self.db.executescript(update)
+			self.db.execute("PRAGMA user_version = {}".format(version))
 	
+	# Appends a new entry to the journal. Any extra args are usually specific to the given `op`, and
+	# will be pickled before storage.
 	def append(self, source, file, op, *args, time = None):
 		cur = self.db.cursor()
 		cur.execute('''
@@ -55,6 +73,7 @@ class Journal:
 		self.db.commit()
 		self.has_changes = True
 
+	# Returns all matching transactions for a given file and op.
 	def get_transactions(self, file, op):
 		cur = self.db.cursor()
 		cur.execute('''
@@ -70,6 +89,12 @@ class Journal:
 		for row in cur.fetchall():
 			yield dict(row, extra = pickle.loads(row['extra']))
 
+	# Sets a checkpoint at the current journal point. This groups all the transactions since the
+	# last checkpoint into one operation and marks them as completely applied to the other
+	# components of the database.
+	#
+	# If no transactions have been done since the last checkpoint, no checkpoint will be created and
+	# this method will return `None`.
 	def checkpoint(self, time = None):
 		if not self.has_changes: return None
 
@@ -85,6 +110,8 @@ class Journal:
 
 		return cur.lastrowid
 
+	# Retrieves the given checkpoint and its transactions. (If `None`, retrieves the latest
+	# transaction.) 
 	def get_checkpoint(self, checkpoint_id):
 		if checkpoint_id is None:
 			checkpoint = self.db.execute('''
@@ -134,6 +161,7 @@ class Journal:
 
 		return checkpoint
 
+	# Retrieves the IDs of all checkpoints in the given order (`'asc'` or `'desc'`).
 	def all_checkpoint_ids(self, *, order = 'asc'):
 		for row in self.db.execute('SELECT checkpoint_id FROM checkpoints ORDER BY checkpoint_id ' + ('DESC' if order == 'desc' else 'ASC')).fetchall():
 			yield row[0]
