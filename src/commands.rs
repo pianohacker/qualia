1ee52e303172506c1de848099bfae7eaaf46f6b2
Jesse Weaver <pianohacker@gmail.com>
2018-02-19 01:24:26 -0700

    First skeleton subcommand: add

---
+++ src/commands.rs
@@ -0,0 +1,53 @@
+use clap::{App, Arg, ArgMatches, SubCommand};
+use std::collections::HashMap;
+use std::fs::metadata;
+use std::sync::Mutex;
+
+use failure::Error;
+
+lazy_static! {
+	static ref COMMAND_MAP: Mutex<HashMap<&'static str, fn (&ArgMatches) -> Result<(), Error>>> = Mutex::new(HashMap::new());
+}
+
+macro_rules! subcommand {
+    ($app:tt, $name:expr, $subcommand:expr, $impl:expr) => {
+        $app = $app.subcommand($subcommand);
+        COMMAND_MAP.lock().unwrap().insert($name, $impl);
+    }
+}
+
+pub fn register<'a, 'b>(mut app: App<'a, 'b>) -> App<'a, 'b> {
+	subcommand!(
+		app,
+		"add",
+		SubCommand::with_name("add").arg(
+			Arg::with_name("filename")
+				.index(1)
+				.required(true)
+				.multiple(true)
+				.takes_value(true)
+		),
+		|matches| {
+			for filename in matches.values_of("filename").unwrap() {
+				metadata(filename).map_err(|e| format_err!("{}: {}", filename, e))?;
+			}
+
+			println!("add!");
+
+			Ok(())
+		}
+	);
+
+	app
+}
+
+pub fn run<'a>(matches: ArgMatches<'a>) -> Result<(), Error> {
+	let command_map = COMMAND_MAP.lock().unwrap();
+
+	let subcmd = matches.subcommand_name().expect("no subcommand given");
+	let command_impl = command_map
+		.get(subcmd)
+		.expect("should not be reached, subcommand given but unknown");
+
+	command_impl(matches.subcommand_matches(subcmd).unwrap())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
 extern crate failure;
+#[macro_use]
+extern crate lazy_static;
 
 use clap::App;
 use failure::Error;
 
+mod commands;
+
 fn main() {
-	main_impl().expect("success");
+	if let Err(e) = main_impl() {
+		eprintln!("{}", e);
+	}
 }
 
 fn main_impl() -> Result<(), Error> {
-	let matches = App::new("qualia")
+	let mut clap_app = App::new("qualia")
 		.version(crate_version!())
 		.author("Jesse Weaver <pianohacker@gmail.com>")
-		.about("Metadata-focused file organizer")
-		.get_matches();
-	println!("yo");
+		.about("Metadata-focused file organizer");
 
-	Ok(())
+	clap_app = commands::register(clap_app);
+
+	let matches = clap_app.get_matches();
+
+	commands::run(matches)
 }
