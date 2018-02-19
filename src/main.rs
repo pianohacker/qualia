1c14942d22569bac14a231dd165a4e01bcd6492a
Jesse Weaver <pianohacker@gmail.com>
2018-02-18 19:29:50 -0700

    Initial version with formatting and command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[macro_use]
+extern crate clap;
+extern crate failure;
+
+use clap::App;
+use failure::Error;
+
 fn main() {
-    println!("Hello, world!");
+	main_impl().expect("success");
+}
+
+fn main_impl() -> Result<(), Error> {
+	let matches = App::new("qualia")
+		.version(crate_version!())
+		.author("Jesse Weaver <pianohacker@gmail.com>")
+		.about("Metadata-focused file organizer")
+		.get_matches();
+	println!("yo");
+
+	Ok(())
 }
