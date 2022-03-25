478efa777b29db9fae00fa743811a8c29ab8225e
Jesse Weaver <pianohacker@gmail.com>
2022-03-24 20:01:53 -0600

    Fix test dependending on loose derive attribute ordering

--- qualia_derive/tests/compile-fail/only-supported-fixed-field-types.rs
+++ qualia_derive/tests/compile-fail/only-supported-fixed-field-types.rs
@@ -1,8 +1,8 @@
 extern crate qualia_derive;
 use qualia_derive::ObjectShape;
 
-#[object_fixed_fields("foo" => 4.5)]
 #[derive(ObjectShape)]
+#[object_fixed_fields("foo" => 4.5)]
 struct FixedFoo {
     a: i64,
     c: String,
